2e1627be6f750754d448a6fd54b3aec56dfb8113
Justin Neubert <neubert_justin@outlook.com>
2025-09-30 12:36:41 +0200

    fix: Correct interface name from WorkerGlobalScope to ServiceWorkerGlobalScope in service worker

--- services/frontend/app/sw.ts
+++ services/frontend/app/sw.ts
@@ -3,12 +3,14 @@ import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "serwist";
 
+/// <reference lib="webworker" />
+
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
 // actual precache manifest. By default, this string is set to
 // `"self.__SW_MANIFEST"`.
 declare global {
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
+  interface ServiceWorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
   }
 }

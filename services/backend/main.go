54db47d4ccec74231e402cbd9f36c9bdefed8ff7
Justin Neubert <neubert_justin@outlook.com>
2026-03-20 11:47:06 +0100

    fix: update version numbers in main.go and package.json to 0.6.0

--- services/backend/main.go
+++ services/backend/main.go
@@ -18,7 +18,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "0.5.0"
+const version string = "0.6.0"
 
 var (
 	configFile = kingpin.Flag("config", "Config file").Short('c').Default("/etc/justscan/config.yaml").String()

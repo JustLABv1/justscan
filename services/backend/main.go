b94310da46ebc7957b53248982ee19efe3730a23
Justin Neubert <neubert_justin@outlook.com>
2026-03-23 16:07:11 +0100

    fix: update version number to 0.10.0 in main.go

--- services/backend/main.go
+++ services/backend/main.go
@@ -18,7 +18,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "0.9.0"
+const version string = "0.10.0"
 
 var (
 	configFile = kingpin.Flag("config", "Config file").Short('c').Default("/etc/justscan/config.yaml").String()

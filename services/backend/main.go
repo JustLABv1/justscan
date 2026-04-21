eae523d9d20b2836c0a073cfba4dc79df71894d0
Justin Neubert <neubert_justin@outlook.com>
2026-04-21 07:59:17 +0200

    chore: update version to 1.6.0-beta.6 in backend and frontend

--- services/backend/main.go
+++ services/backend/main.go
@@ -21,7 +21,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "1.6.0-beta.5"
+const version string = "1.6.0-beta.6"
 
 var (
 	configFile = kingpin.Flag("config", "Config file").Short('c').Default("/etc/justscan/config.yaml").String()

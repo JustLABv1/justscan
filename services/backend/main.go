447ee0d8a40d599be86c8139b7abb2c06f68b038
Justin Neubert <neubert_justin@outlook.com>
2026-06-11 09:38:00 +0200

    bump version to 2.0.0

--- services/backend/main.go
+++ services/backend/main.go
@@ -22,7 +22,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "2.0.0-beta.16"
+const version string = "2.0.0"
 
 var (
 	configFile = kingpin.Flag("config", "Config file").Short('c').Default("/etc/justscan/config.yaml").String()
